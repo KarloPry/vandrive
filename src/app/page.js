edc8e05ec269144422f7c3e7f0b01a01ec947d5d
ElYizuz29 <amorales130@alumnos.uaq.mx>
2024-04-20 08:55:37 -0600

    Poner la funcion de los botones de Auth0

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import ProfileClient from "../../components/ProfileClient";
+'use client'
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* Este es el botón de Login Auth0 y el de Logout Auth0 */}
-      <a href="/api/auth/login">Login</a>
-      <a href="/api/auth/logout">Logout</a>
-      <ProfileClient/>
-    </main>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/en/LandingPage");
+  }, []);
+
+  return null;
 }
