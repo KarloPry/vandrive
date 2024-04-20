42b87f4ef3bc3881fcc5129e65b53e4b949cce03
ElYizuz29 <amorales130@alumnos.uaq.mx>
2024-04-20 06:15:11 -0600

    Update 3d models

--- src/app/es/LandingPage/page.jsx
+++ src/app/es/LandingPage/page.jsx
@@ -3,10 +3,8 @@
 import DCardPricing from '@/components/DCardPricing'
 import { LPContactUS } from '@/components/LPContactUs'
 import { LPFooter } from '@/components/LPFooter'
-import { LPHeader } from '@/components/LPHeader'
 import { LPLogo } from '@/components/LPLogo'
 import LPNavBar from '@/components/LPNavBar'
-import { LPPrincing } from '@/components/LPPricing'
 import { LPTestimonial } from '@/components/LPTestimonial'
 import Vanlejandro from '@/components/DHeader'
 import React from 'react'
@@ -17,7 +15,7 @@ const page = () => {
         <LPNavBar/>
         <Vanlejandro/>
         <LPLogo/>
-        <div className="mx-auto mb-12 max-w-lg text-center md:mb-18 lg:mb-20">
+        <div id='Pricing' className="mx-auto mb-12 max-w-lg text-center md:mb-18 lg:mb-20">
           <h1 className="mb-5 text-5xl font-bold md:mb-6 md:text-7xl lg:text-8xl text-blue-950">Planes de Precios</h1>
         </div>
         <DCardPricing/>
