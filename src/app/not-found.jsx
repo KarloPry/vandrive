772a97d0eb9fc5fb4b3b5eba89a4218ffae5e49c
ElYizuz29 <amorales130@alumnos.uaq.mx>
2024-04-20 06:19:26 -0600

    Added notfound page with 3d

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+import Spline from '@splinetool/react-spline';
+
+export default function NotFound() {
+  return (
+    <Spline scene="https://prod.spline.design/M2yZCkiuAW10Y1B9/scene.splinecode" />
+  );
+}
