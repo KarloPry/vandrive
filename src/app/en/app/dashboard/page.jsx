e863140831d4a0251c2d6bc1dc4a8335177c6fba
ElYizuz29 <amorales130@alumnos.uaq.mx>
2024-04-20 01:18:34 -0600

    Added new SideBar and es and en Dashboards

--- src/app/en/app/dashboard/page.jsx
+++ src/app/en/app/dashboard/page.jsx
@@ -1,6 +1,13 @@
-"use client"
+"use client";
 import { Avatar, Button } from "@nextui-org/react";
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
 import { LineGraph } from "@/components/LineGraph";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -11,53 +18,51 @@ const columns = [
   { name: "INTERMEDIATE POINTS", uid: "intPoint" },
   { name: "DISTANCE", uid: "distance" },
   { name: "PASSENGERS NUMBER", uid: "passNumber" },
-  { name: "ACTIONS", uid: "actions" }
-]
+  { name: "ACTIONS", uid: "actions" },
+];
 
 const trips = [
   {
-    "id": "1",
-    "StartAddress": "Calle 1, Ciudad A",
-    "EndAddress": "Avenida Principal, Ciudad B",
-    "IntermediatePoints": 2,
-    "Distance": 10.5,
-    "PassengersNumber": 3
+    id: "1",
+    StartAddress: "Calle 1, Ciudad A",
+    EndAddress: "Avenida Principal, Ciudad B",
+    IntermediatePoints: 2,
+    Distance: 10.5,
+    PassengersNumber: 3,
   },
   {
-    "id": "2",
-    "StartAddress": "Avenida Central, Ciudad C",
-    "EndAddress": "Calle 5, Ciudad D",
-    "IntermediatePoints": 1,
-    "Distance": 8.2,
-    "PassengersNumber": 2
+    id: "2",
+    StartAddress: "Avenida Central, Ciudad C",
+    EndAddress: "Calle 5, Ciudad D",
+    IntermediatePoints: 1,
+    Distance: 8.2,
+    PassengersNumber: 2,
   },
   {
-    "id": "3",
-    "StartAddress": "Bulevar Norte, Ciudad E",
-    "EndAddress": "Calle 10, Ciudad F",
-    "IntermediatePoints": 3,
-    "Distance": 15.7,
-    "PassengersNumber": 4
+    id: "3",
+    StartAddress: "Bulevar Norte, Ciudad E",
+    EndAddress: "Calle 10, Ciudad F",
+    IntermediatePoints: 3,
+    Distance: 15.7,
+    PassengersNumber: 4,
   },
   {
-    "id": "4",
-    "StartAddress": "Calle 20, Ciudad G",
-    "EndAddress": "Avenida Sur, Ciudad H",
-    "IntermediatePoints": 0,
-    "Distance": 5.3,
-    "PassengersNumber": 1
+    id: "4",
+    StartAddress: "Calle 20, Ciudad G",
+    EndAddress: "Avenida Sur, Ciudad H",
+    IntermediatePoints: 0,
+    Distance: 5.3,
+    PassengersNumber: 1,
   },
   {
-    "id": "5",
-    "StartAddress": "Avenida Este, Ciudad I",
-    "EndAddress": "Calle 15, Ciudad J",
-    "IntermediatePoints": 2,
-    "Distance": 12.1,
-    "PassengersNumber": 3
-  }
+    id: "5",
+    StartAddress: "Avenida Este, Ciudad I",
+    EndAddress: "Calle 15, Ciudad J",
+    IntermediatePoints: 2,
+    Distance: 12.1,
+    PassengersNumber: 3,
+  },
 ];
-
-
 
 export default function Page() {
   // const [trips, setTrips] = useState();
@@ -65,74 +70,70 @@ export default function Page() {
   useEffect(() => {
     //
     // setTrips(respuesta de la API)
-  }, [])
+  }, []);
 
   const renderCell = (trip, columnKey) => {
     switch (columnKey) {
       case "startAdrress":
-        return (
-          <>{trip.StartAddress}</>
-        );
+        return <>{trip.StartAddress}</>;
       case "endAddress":
-        return (
-          <>{trip.EndAddress}</>
-        );
+        return <>{trip.EndAddress}</>;
       case "intPoint":
-        return (
-          <>-</>
-        );
+        return <>-</>;
       case "distance":
-        return (
-          <>{trip.Distance}</>
-        );
+        return <>{trip.Distance}</>;
       case "passNumber":
-        return (
-          <>{trip.PassengersNumber}</>
-        );
+        return <>{trip.PassengersNumber}</>;
       case "actions":
         return (
-          <Button isIconOnly color="primary"><span className="material-symbols-rounded">visibility</span></Button>
+          <Button isIconOnly color="primary">
+            <span className="material-symbols-rounded !text-white">
+              visibility
+            </span>
+          </Button>
         );
     }
-  }
+  };
 
   return (
     <div className="flex flex-col flex-1 p-4 bg-[#F2F2F2] gap-4 overflow-y-auto overflow-x-hidden">
-      <div className="w-full flex items-center justify-end gap-4">
-        <Button
-          onPress={() => router.push("/en/app/map")}
-          color="primary"
-          startContent={<span className="material-symbols-rounded !text-white">add</span>}
-        >Add trip</Button>
-        <div className="flex gap-2 items-center justify-end">
-          <Avatar showFallback src='https://images.unsplash.com/broken' />
-          <p>Diego Martínez García</p>
-        </div>
-      </div>
       <div className="bg-white p-4 rounded-xl">
-        <div className="flex gap-2 items-center">
-          <h2 className="text-2xl inline-block">Trip record</h2>
-          <span className="material-symbols-rounded inline-block">history</span>
-        </div>
-
-        <Table aria-label="adiwmaldw">
-          <TableHeader columns={columns}>
-            {
-              (column) => (
-                <TableColumn key={column.uid} align="center">
-                  {column.name}
-                </TableColumn>
-              )
+        <div className="flex flex-row justify-between mb-4">
+          <div className="flex gap-2 items-center">
+            <h2 className="text-2xl inline-block">Trip record</h2>
+            <span className="material-symbols-rounded inline-block">
+              history
+            </span>
+          </div>
+          <Button
+            onPress={() => router.push("/en/app/map")}
+            color="primary"
+            className="text-white"
+            startContent={
+              <span className="material-symbols-rounded !text-white">add</span>
             }
+          >
+            Add trip
+          </Button>
+        </div>
+        <Table aria-label="adiwmaldw" align="center">
+          <TableHeader columns={columns} className="">
+            {(column) => (
+              <TableColumn key={column.uid} align="center" className="bg-red-100">
+                {column.name}
+              </TableColumn>
+            )}
           </TableHeader>
           <TableBody items={trips}>
-            {
-              (trip) => (
-                <TableRow key={trip.id}>
-                  {(columnKey) => <TableCell key={columnKey}>{renderCell(trip, columnKey)}</TableCell>}
-                </TableRow>
-              )
-            }
+            {(trip) => (
+              <TableRow key={trip.id} align="center">
+                {(columnKey) => (
+                  <TableCell key={columnKey}>
+                    {renderCell(trip, columnKey)}
+                  </TableCell>
+                )}
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </div>
@@ -140,7 +141,14 @@ export default function Page() {
         <div className="h-96 flex-1 bg-white rounded-2xl p-4">
           <LineGraph
             chartData={[1, 2, 3, 5, 6, 7]}
-            chartLabels={["27 Jun", "10 Jul", "15 Ago", "20 Sep", "24 Oct", "30 Nov"]}
+            chartLabels={[
+              "27 Jun",
+              "10 Jul",
+              "15 Ago",
+              "20 Sep",
+              "24 Oct",
+              "30 Nov",
+            ]}
             chartTitle={"Km/Trip"}
             color={"#E26969"}
             bgColor={"#BB3535"}
@@ -149,7 +157,14 @@ export default function Page() {
         <div className="h-96 flex-1 bg-white rounded-2xl p-4">
           <LineGraph
             chartData={[15, 17, 20, 20, 10, 12]}
-            chartLabels={["27 Jun", "10 Jul", "15 Ago", "20 Sep", "24 Oct", "30 Nov"]}
+            chartLabels={[
+              "27 Jun",
+              "10 Jul",
+              "15 Ago",
+              "20 Sep",
+              "24 Oct",
+              "30 Nov",
+            ]}
             chartTitle={"Passengers/Trip"}
             color={"#416C95"}
             bgColor={"#16436D"}
